3f14a502f0c562e0d8e6b6a34c7cfb9d9e78ec56
sunrisedangercobb <sunrisedangercobb@gmail.com>
2016-08-23 19:53:39 -0700

    Update query.php
    fixing spacing.

--- query.php
+++ query.php
@@ -2,36 +2,36 @@
 
 function Query($sql)
 {
-	$db_conn = mysqli_connect('hostname', 'user', 'pass', 'database');
+  $db_conn = mysqli_connect('hostname', 'user', 'pass', 'database');
 
-	if (!$db_conn) {
+  if (!$db_conn) {
     echo "Can't connect to localhost. Error: " . mysqli_connect_error();
     exit();
-	}
+  }
 
-	// Turn on auto-commit
-	mysqli_autocommit($db_conn, TRUE);
+  // Turn on auto-commit
+  mysqli_autocommit($db_conn, TRUE);
 
-	$db_result = mysqli_query($db_conn, $sql);
+  $db_result = mysqli_query($db_conn, $sql);
 
-	if (!$db_result) {
-		// Close the connection
-		mysqli_close($db_conn);
-		return null;
-	}
+  if (!$db_result) {
+    // Close the connection
+    mysqli_close($db_conn);
+    return null;
+  }
 
     $result = null;
 
     $part_1 = explode(' ', strtoupper($sql));
 
     if ($part_1[0] === 'SELECT') {
-    	$result = array();
+      $result = array();
 
-    	for ($i = 0; $i < mysqli_num_rows($db_result); $i++)
-    	{
-    		$row = mysqli_fetch_assoc($db_result);
-    		$result[$i] = $row;
-    	}
+      for ($i = 0; $i < mysqli_num_rows($db_result); $i++)
+      {
+        $row = mysqli_fetch_assoc($db_result);
+        $result[$i] = $row;
+      }
     }
 
     else if ($part_1[0] === 'INSERT') {
@@ -39,40 +39,40 @@ function Query($sql)
         error_log("insert last id $result");
     }
 
-	// Close the connection
-	mysqli_close($db_conn);
+  // Close the connection
+  mysqli_close($db_conn);
 
-	return $result;
+  return $result;
 }
 
 
 function SanitizeSQL($value)
 {
-    $value = ''.$value;
-    $value = str_replace("'", "''", $value);
-    return $value;
+  $value = ''.$value;
+  $value = str_replace("'", "''", $value);
+  return $value;
 }
 
 
 function SanitizeOrNull($value)
 {
-    if ($value === null || $value === "")
-    {
-        return "NULL";
-    }
+  if ($value === null || $value === "")
+  {
+    return "NULL";
+  }
 
-    $value = "'".SanitizeSQL($value)."'";
+  $value = "'".SanitizeSQL($value)."'";
 
-    return $value;
+  return $value;
 }
 
 function NumberOrNull($value)
 {
-    if ($value === null || $value === "")
-    {
-        return "NULL";
-    }
+  if ($value === null || $value === "")
+  {
+    return "NULL";
+  }
 
-    return $value;
+  return $value;
 }
 ?>
